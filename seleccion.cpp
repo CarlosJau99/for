38ccc8970b71b4e19b24cc7ca782f8d3b28b41c4
carlosjauregui99 <cjaureguil@miumg.edu.gt>
2024-02-29 21:31:28 -0600

    for

---
+++ seleccion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+main(){
+	//i++ ,i=+1 ,i=i+1
+	int contar=0;
+	for (int i=0;i<=10;i++){
+		cout<<i<<endl;
+		contar++;
+	}
+	cout<<"ciclos:"<<contar<<endl;
+	
+	
+	
+	
+	system("pause");
+}
